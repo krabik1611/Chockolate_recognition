93171ee214d885232e08fdd0d05578be4a4a0cf8
krabby1611 <danilmelnikov11@gmail.com>
2020-09-12 11:40:00 +0500

    NONE

--- test.py
+++ test.py
@@ -18,18 +18,16 @@ class PoolNet(nn.Module):
         return x
 
 net = PoolNet()
-# print(a.size())
-# out = conv(a)
-# print(out.size())
+pool = nn.MaxPool2d(2,2)
 img = cv.cvtColor(cv.imread("Data/data_image/1.jpg"),cv.COLOR_BGR2RGB)
-# plt.imshow(img)
-# plt.show()
 img_tensor = torch.Tensor(img).T.unsqueeze(dim=0)
 out = net(img_tensor).squeeze(0).T.detach().numpy()
-
-
-plt.subplot(1,2,1),plt.imshow(img)
-plt.title("Orig"), plt.xticks([]),plt.yticks([])
-plt.subplot(1,2,2),plt.imshow(out)
-plt.title("Pooling"), plt.xticks([]),plt.yticks([])
-plt.show()
+print(img.shape)
+print(out.shape)
+#
+# plt.subplot(1,2,1),plt.imshow(img)
+# plt.title("Orig"), plt.xticks([]),plt.yticks([])
+# plt.subplot(1,2,2),plt.imshow(out)
+# plt.title("Pooling"), plt.xticks([]),plt.yticks([])
+# plt.show()
+# img_p = pool(img)
