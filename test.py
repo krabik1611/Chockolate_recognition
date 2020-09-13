f13e5ca920b17b414eca70a0efe14723b71d047d
krabby1611 <danilmelnikov11@gmail.com>
2020-09-13 11:11:21 +0500

    add test.py

--- test.py
+++ test.py
@@ -1,34 +1,15 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
+import os
 import cv2 as cv
-import matplotlib.pyplot as plt
 
-class PoolNet(nn.Module):
-    def __init__(self):
-        super(PoolNet,self).__init__()
-        self.conv1 = nn.Conv2d(3,6,3)
-        self.conv2 = nn.Conv2d(6,3,3)
-        self.pool = nn.MaxPool2d(2,2)
+data = "Data/data_image/"
+files = os.listdir(data)
+average = [0,0]
+for file in files:
+    filename = data + file
+    img = cv.imread(filename)
+    y, x,_ = img.shape
+    # print("({},{})".format(x, y))
+    average[0] += x / len(files)
+    average[1] += y / len(files)
 
-
-    def forward(self,x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        return x
-
-net = PoolNet()
-pool = nn.MaxPool2d(2,2)
-img = cv.cvtColor(cv.imread("Data/data_image/1.jpg"),cv.COLOR_BGR2RGB)
-img_tensor = torch.Tensor(img).T.unsqueeze(dim=0)
-out = net(img_tensor).squeeze(0).T.detach().numpy()
-# print(img.shape)
-# print(out.shape)
-#
-plt.subplot(1,2,1),plt.imshow(img)
-plt.title("Orig"), plt.xticks([]),plt.yticks([])
-plt.subplot(1,2,2),plt.imshow(out)
-plt.title("Pooling"), plt.xticks([]),plt.yticks([])
-# img_p = pool(img)
-# plt.imshow(img)
-plt.show()
+print(average)
