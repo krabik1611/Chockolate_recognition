24b49f7590925dc0bbfe52b252c4d60c64e551cf
krabby1611 <danilmelnikov11@gmail.com>
2020-09-14 18:56:11 +0500

    write test action in main.py

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-import model
-import os
-import torch
+import imagesAPI as api
 import matplotlib.pyplot as plt
-import cv2 as cv
-import numpy as np
-import base as bd
+import os
+import csv
+import numpy.linalg as lin
 
-# data_dir = "Data/data_image/"
-# list_files = os.listdir(data_dir)
-# mod_dir = "Data/mod_image/"
-# mod_files = os.listdir(mod_dir)
-# net = model.Net()
-base = bd.DataBase()
-# color = ['k','g','r','c','m','y']
-# count = 1
-# for file in list_files:
-#     filename = data_dir + file
-#     # print(filename)
-#     img = cv.cvtColor(cv.imread(filename), cv.COLOR_BGR2RGB)
-#     orig = net(model.img2tensor(img))
-#     dict_data = {
-#         "id": file[:file.find(".")],
-#         "filename": filename,
-#         "data": model.tensor2img(orig)
-#     }
-#     base.writeDB(dict_data)
 
-rows = base.readDB()
-print(len(rows))
-# for row in rows:
-#     print(row[0], row[1], row[2])
+def getState():
+    global count
+    global allCount
+    proc = (count/allCount)*100
+    stat = '[%s%s]' %("##"*(int(proc)//10),'--'*(10-(int(proc)//10)))
+    string = 'Complete %22s %i%% %i/%i' %(stat,proc,count,allCount)
+    print(string)
+
+if __name__ == '__main__':
+    data_dir = "Data/data_image/"
+    action = api.MainActive()
+    count = 1
+    list_files = os.listdir(data_dir)
+    allCount = len(list_files)
+    # with open("data.csv", "w") as f:
+    #     writer = csv.writer(f)
+    #     for file in list_files:
+    #         filename = data_dir + file
+    #         img = action.getImage(filename)
+    #         out = action.runNet(img)
+    #         id = file[:file.find(".")]
+    #         data_list = [id, filename, out]
+    #         writer.writerow(data_list)
+    #         getState()
+    #         count += 1
+    for file in list_files:
+
+        if 1:
+            filename = data_dir + file
+            img = action.getImage(filename)
+            out = action.runNet(img)
+            # dict_data = {
+            #             "id":file[:file.find('.')],
+            #             "filename":filename,
+            #             "data":out
+            # }
+            # action.writeDB(dict_data)
+            # print(out.mean())
+            # print(out)
+            plt.plot(out)
+            count +=1
+        else:
+            break
+    plt.show()
+        # getState()
+        # count += 1
