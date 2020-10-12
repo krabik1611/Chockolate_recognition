233c7e700371b4f36081dd669b764cd83f6fd0f7
krabby1611 <danilmelnikov11@gmail.com>
2020-10-12 18:36:34 +0500

    move Net to model.py

--- main.py
+++ main.py
@@ -6,57 +6,48 @@ import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
+from model import Net,getData
+import cv2 as cv
 
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(2704, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.sigmoid(self.conv2(x)))
-        # x = x.view(-1, 2704)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        return x
+device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
 transform = transforms.Compose([
+                transforms.ToPILImage(),
                 transforms.Resize((64,64)),
                 transforms.ToTensor(),
                 # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
 ])
 
-device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-
-trainset = dset.ImageFolder(root="new_model/Image/",transform=transform)
-
-trainloader = torch.utils.data.DataLoader(trainset,shuffle=True,batch_size=1)
+trainloader = getData()
 
 net = Net().to(device)
 net.load_state_dict(torch.load("model.th"))
+#
+# out = []
+# with torch.no_grad():
+#     for i,data in enumerate(trainloader):
+#         image,_ = data
+#         outputs = net(image.to(device))
+#         out.append(outputs)
+#
+#         if i == 2:
+#             break
+#
+# for img in out:
+#     vector = img.view(-1).cpu().detach().numpy()
+#
+#     plt.hist(vector)
+#     print(vector.shape)
+# # plt.xticks()
+# plt.show()
+img = "test_image/1.jpg"
+img = cv.cvtColor(cv.imread(img),cv.COLOR_BGR2RGB)
+# plt.imshow(img)
+# plt.show()
 
-out = []
-with torch.no_grad():
-    for i,data in enumerate(trainloader):
-        image,_ = data
-        outputs = net(image.to(device))
-        out.append(outputs)
+tens = transform(img)
 
-        if i == 2:
-            break
-for img in out:
-    vector = img.view(-1).cpu().detach().numpy()
-    plt.plot(vector)
-    print(vector.max())
-# plt.xticks()
-plt.show()
+out = net(tens.to(device))
+print(out.shape)
---
+++ model.py
@@ -0,0 +1,40 @@
+import torchvision.datasets as dset
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(2704, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        if len(x.size()) != 4:
+            x = x.unsqueeze(0)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(torch.sigmoid(self.conv2(x)))
+        x = x.view(-1, 2704)
+        x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        return x.view(-1).cpu().detach().numpy()
+
+
+def getData():
+    transform = transforms.Compose([
+                    transforms.Resize((64,64)),
+                    transforms.ToTensor(),
+                    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+    ])
+    trainset = dset.ImageFolder(root="new_model/Image/",transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset,shuffle=True,batch_size=1)
+    return trainloader
